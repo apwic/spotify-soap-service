6db27a4b7317979956a7e796f1f824be4f62d4f0
azkazkazka <sarah_azka@yahoo.com>
2022-12-01 17:06:18 +0700

    fix: soap sql hehe

--- sepotipayi_soap.sql
+++ sepotipayi_soap.sql
@@ -1,19 +1,84 @@
-CREATE DATABASE sepotipayi_soap;
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 01, 2022 at 05:05 PM
+-- Server version: 8.0.31-0ubuntu0.20.04.1
+-- PHP Version: 8.0.25
 
-CREATE TABLE logging (
-  id int NOT NULL AUTO_INCREMENT,
-  description char(255) NOT NULL,
-  ip char(16) NOT NULL,
-  endpoint char(255) NOT NULL,
-  requested_at timestamp NOT NULL,
-  PRIMARY KEY (id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE subscription (
-  creator_id int NOT NULL,
-  subscriber_id int NOT NULL,
-  creator_name char(255) NOT NULL,
-  subscriber_name char(255) NOT NULL,
-  status char(255) NOT NULL DEFAULT 'PENDING',
-  CONSTRAINT pk_id PRIMARY KEY (creator_id, subscriber_id)
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sepotipayi_soap`
+--
+CREATE DATABASE IF NOT EXISTS `sepotipayi_soap` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `sepotipayi_soap`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logging`
+--
+
+CREATE TABLE `logging` (
+  `id` int NOT NULL,
+  `description` char(255) NOT NULL,
+  `ip` char(16) NOT NULL,
+  `endpoint` char(255) NOT NULL,
+  `requested_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription`
+--
+
+CREATE TABLE `subscription` (
+  `creator_id` int NOT NULL,
+  `subscriber_id` int NOT NULL,
+  `creator_name` char(255) NOT NULL,
+  `subscriber_name` char(255) NOT NULL,
+  `status` char(255) NOT NULL DEFAULT 'PENDING'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logging`
+--
+ALTER TABLE `logging`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscription`
+--
+ALTER TABLE `subscription`
+  ADD PRIMARY KEY (`creator_id`,`subscriber_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logging`
+--
+ALTER TABLE `logging`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
